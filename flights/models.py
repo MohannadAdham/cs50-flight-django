692cfbb94c75e0486868e9d9c094f0d3ee083160
Mohannad Adham <mohannad.adm@gmail.com>
2020-12-17 06:00:05 +0100

    Make migrations and migrate the flights model

--- flights/models.py
+++ flights/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Airport(models.Model):
-    code = madels.CharField(max_length=3)
+    code = models.CharField(max_length=3)
     city = models.CharField(max_length=64)
 
     def __str__(self):
