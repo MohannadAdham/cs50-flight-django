4861031e0621dcd1b1dbee88708bb77319958555
Mohannad Adham <mohannad.adm@gmail.com>
2020-12-18 06:15:41 +0100

    define the index and login_view functions in the views.py of the user app.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+
+def login_view(request):
+    return render(request, "uresrs/login.html")
