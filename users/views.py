bd30d5b587b3730b2cc43a16cf3c035ea82a19de
Mohannad Adham <mohannad.adm@gmail.com>
2020-12-18 06:55:35 +0100

    Implement user login authentification.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -6,6 +7,22 @@ from django.urls import reverse
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
+    else: 
+        return render(request, "users/user.html")
 
 def login_view(request):
-    return render(request, "uresrs/login.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid credentials."
+            })
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    pass
